1eef508e4dd6f3cbecc1cfc45a5647ecc05ed482
Marcos <00001066767658sp@al.educacao.sp.gov.br>
2022-01-02 15:37:45 -0300

    validacao de tipo

--- progressBar.py
+++ progressBar.py
@@ -1,11 +1,13 @@
+from collections.abc import Iterable
+
 def progressBar(itens,length=50,prefixo="Progress",sufixo="Complete",preenchimento="#",onProgress=""):
-    
+
     if(length > 100 or length < 10):
         length = 50
 
     # Pega o número total de items de um iterable como uma list
     totalItems = len(itens) 
-    #atualziar  a barra de progresso
+    #atualziar  a barra de progress
     def atualiza(itemAtual):
         #Calculo da porcentagem da barra de progresso
         porcentagem = "{0:.0f}".format((itemAtual / float(totalItems)*100))
@@ -23,10 +25,19 @@ def progressBar(itens,length=50,prefixo="Progress",sufixo="Complete",preenchimen
     atualiza(0)
 
     # atualização da barra de progresso feito a cada atualização
-    for index,value in enumerate(itens):
-        # retorna valor para utilizar numa estrutura de repetição
-        yield value
-        atualiza(index + 1)
-
+    # e validacao dos tipos range ou interable
+    try:
+        if isinstance(itens,Iterable):
+            for index,value in enumerate(itens):
+                # retorna valor para utilizar numa estrutura de repetição
+                yield value
+                atualiza(index + 1)
+        else:
+            for index in itens:
+                yield index
+                atualiza(index +1 )
+    except:
+        print("only interable or range allowed!")
+        
     if(onProgress != "\n"):
         print()
